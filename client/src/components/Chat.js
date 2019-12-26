9dce94b4b7ddeba4699d18bb9b5d79f29cf5a674
Pankajan Satkunam <pankajan05@gmail.com>
2019-12-26 12:33:21 +0530

    add server

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -5,3 +5,5 @@ const Chat = () => {
         <h1>Chat</h1>
     )
 }
+
+export default Chat;
--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Join = () => {
+    const [name, setName] = useState('');
+    const [rom, setRoom] = useState('');
+    
     return (
         <h1>Join</h1>
     )
 }
+
+export default Join;
