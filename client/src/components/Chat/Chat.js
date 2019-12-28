17ebb84cb7823215b3859cf5cd1466e59af375ae
Pankajan Satkunam <pankajan05@gmail.com>
2019-12-28 17:41:45 +0530

    Add the message emiting

---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,60 @@
+import React,{ useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+
+let socket;
+
+const Chat = ({ location }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+    const ENDPOINT = 'localhost:5000';
+
+    useEffect(()=>{
+        const {name, room} = queryString.parse(location.search);
+
+        socket = io(ENDPOINT);
+        setRoom(room);
+        setName(name);
+
+        socket.emit('join', { name, room },()=>{
+            
+        });
+
+        return () => {
+            socket.emit('disconnect');
+
+            socket.off();
+        }
+    }, [ENDPOINT, location.search]);
+
+    //add the admin message to the messages
+    useEffect(() => {
+        socket.on('message',(message)=>{
+            setMessages([...messages,message]);
+        });
+    },[messages]);
+
+    //emit the user type messages
+   const sendMessage = (event) => {
+       event.preventDefault();
+
+       if (message){
+           socket.emit('sendMessage',message, ()=> setMessage(''));
+       }
+   }
+console.log(message, messages);
+
+    return (
+        <div className="outerContainer">
+            <div className="container">
+                <input value={message} 
+                    onChange={(event)=> setMessage(event.target.value)} 
+                    onKeyPress={event => event.key=== 'Enter' ? sendMessage(event) : null}></input>
+            </div>
+        </div>
+    )
+}
+
+export default Chat;
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUserInRoom} = require('./users');
+
 const PORT = process.env.PORT || 5000
 
 const router = require('./router');
@@ -11,13 +13,38 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-    console.log('we have a new connection');
+    //emit message when the user join to the room
+    socket.on('join', ({name,room}, callback) => {
+        const {error, user } = addUser({id:socket.id, name, room});
 
+        if(error) return callback(error);
+
+        socket.emit('message', {user:'admin', text:'${user.name} welcome to the room ${user.room}'});
+        socket.broadcast.to(user.room).emit('message', {user: 'admin', text: '${user.name}, has joined!'});
+
+
+        socket.join(user.room);
+
+        callback();
+    });
+
+    //emit when the user type
+    socket.on('sendMessage', (message,callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message',{ user: user.name, text: message});
+
+        callback();
+    });
+
+
+    //when user leave emit this message
     socket.on('disconnect', () => {
         console.log('User had left..');
     });
 });
 
+//routing 
 app.use(router);
 
 server.listen(PORT, () => console.log('Server has started on port ${PORT}'));
